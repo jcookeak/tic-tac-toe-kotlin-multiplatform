17d7c48a80c1ca0871886bb2526c382dbc995f38
jcookeak <j.cookeakaiwa@gmail.com>
2023-02-20 19:05:53 -0500

    add svelte project

---
+++ web/svelte/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+
+    import {Greeting} from 'tic-tac-toe-game'
+
+    //
+    // let greeter = new game.KotlinGreeter()
+    let greeting = new Greeting.greeting()
+
+    // let greeting = 'hello world!'
+
+</script>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>{greeting}</p>
